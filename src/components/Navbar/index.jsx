7fd210c05823856feff14ba3b3371b79e6e822ed
m-tessier <morgane.tessier22@gmail.com>
2021-06-07 16:02:25 +0200

    add navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,30 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className="navbar d-flex justify-content-around">
-
-    <h1 className="navbar-brand"> ImmoCoin </h1>
-
-    <Link to="/">
-      Home
-    </Link>
-
-    <Link to="/register">
-      Register
-    </Link>
-    
-    <Link to="/user">
-      Profile
-    </Link>
-    
-    <button>
-      Log out
-    </button>
-    
-    <Link to="/login">
-      Login
-    </Link>
-
+  <nav>
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-baseline">
+        <h1 className="navbar-brand"><Link to="/">ImmoCoin</Link></h1>
+        <div className="col-3">
+          <ul className="d-flex justify-content-between">
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/user">Profil</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </nav>
 );
 

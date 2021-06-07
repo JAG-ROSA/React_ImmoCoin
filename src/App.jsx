49ab486f5c195545d56dea2de98ca5fae76bc1a3
JAG-ROSA <76993925+JAG-ROSA@users.noreply.github.com>
2021-06-07 15:55:23 +0200

    Update home page content (#3)
    * add Router in App (#1)
    
    * Add all nav bar components (#2)
    
    * create Navbar component
    
    * add boostrap react
    
    * update component route
    
    * create Home, Login, Profile & Register page
    
    * create Footer component
    
    * update component routes
    
    * update Home page content
    
    * update config file content
    
    * add hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "components/Navbar";
+import Home from "pages/Home";
+import Register from "pages/Register";
+import Profile from "pages/Profile";
+import Login from "pages/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/user" exact>
+          <Profile />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </main>
+  </Router>
+);
 
 export default App;

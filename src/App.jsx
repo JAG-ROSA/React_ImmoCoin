d4d0bd82069c71208d81834e20885a2b720c183b
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-07 11:38:29 +0200

    update component route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "components/Navbar";
+import Home from "pages/Home";
+import Register from "pages/Register";
+import Profile from "pages/Profile";
+import Login from "pages/Login";
 
 const App = () => (
   <Router>

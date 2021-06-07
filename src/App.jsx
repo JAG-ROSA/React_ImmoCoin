17f30249296f14de7339cf953944f1c582d35257
JAG-ROSA <76993925+JAG-ROSA@users.noreply.github.com>
2021-06-07 16:01:08 +0200

    Revert "Update home page content (#3)"
    This reverts commit 49ab486f5c195545d56dea2de98ca5fae76bc1a3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "components/Navbar";
-import Home from "pages/Home";
-import Register from "pages/Register";
-import Profile from "pages/Profile";
-import Login from "pages/Login";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <main>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/user" exact>
-          <Profile />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </main>
-  </Router>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;

2e6240580d106b2e54aa0d781323b1ddc21e3e7b
m-tessier <morgane.tessier22@gmail.com>
2021-06-07 20:02:16 +0200

    fixed navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav>
-    <div className="container">
-      <div className="d-flex justify-content-between align-items-baseline">
-        <h1 className="navbar-brand"><Link to="/">ImmoCoin</Link></h1>
-        <div className="col-3">
-          <ul className="d-flex justify-content-between">
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/user">Profil</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
+  <header>
+    <nav>
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-baseline">
+          <h1 className="navbar-brand"><Link to="/">ImmoCoin</Link></h1>
+          <div className="col-3">
+            <ul className="d-flex justify-content-between">
+              <li><Link to="/register">S'inscrire</Link></li>
+              <li><Link to="/user">Profil</Link></li>
+              <li><Link to="/login">Se connecter</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 );
 
 export default Navbar;

985bd64724dbc5ebd55505d63f91a34df3db0cda
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-07 20:06:35 +0200

    Front home (#10)
    * add button css
    
    * fixed footer
    
    * fixed navbar
    
    * update home page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => (
-  <footer className="my-bg-primary">
-    <div className="container">
-      <div className="d-flex justify-content-between align-items-end">
-        <div>
-          <Link to="/" className="title-footer">ImmoCoin</Link>
-          <p class="my-text-light">© 2021 ImmoCoin</p>
+  <div className="pt-70">
+    <footer className="my-bg-primary p-4">
+      <div className="container">
+        <div class="d-flex justify-content-between align-items-end">
+          <div>
+            <Link to="/" className="title-footer link">ImmoCoin</Link>
+            <p class="my-text-light">© 2021 ImmoCoin</p>
+          </div>
+          <p class="my-text-light">Carole Meney, Arnaud Gossard, Martin Forget et Morgane Tessier</p>
         </div>
-        <p class="my-text-light">Carole Meney, Arnaud Gossard, Martin Forget et Morgane Tessier</p>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 );
 
 export default Footer;

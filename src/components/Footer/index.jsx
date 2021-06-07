a50cdc15b71617b66f3ef289e6fd65f27d84997f
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-07 16:09:52 +0200

    Navbar and Footer (#6)
    * add fonts Open_Sans and Rajdhani
    
    * add footer
    
    * add navbar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
-
+  <footer className="my-bg-primary">
+    <div className="container">
+      <div class="d-flex justify-content-between align-items-end">
+        <div>
+          <Link to="/" className="title-footer">Immo</Link>
+          <p class="my-text-light">© 2021 Immo</p>
+        </div>
+        <p class="my-text-light">Carole Meney, Arnaud Gosard, Martin Forget et Morgane Tessier</p>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;

afd5d8d1dd9f6445dc13666a1f7dccb5cfa0873f
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-08 18:21:21 +0200

    Add Button on User Profile Page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,29 @@
+// import React, { useState } from "react";
 import React from "react";
+import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
 
 const Profile = () => {
 
+  const [isUserProfile, setIsUserProfile] = useState(true);
+  const [isCreatePost, setIsCreatePost] = useState(false);
+
+  const onUserChoice = () => {
+    setIsUserProfile(!isUserProfile);
+    setIsCreatePost(!isCreatePost);
+  };
+
+
+  // const onClick =
   return (
-    <div>
-      Profile
-    </div>
+    <Container>
+      <h2>Profile</h2>
+      <ButtonGroup>
+        <Button onClick={}>Profile Utilisateur</Button>
+        <Button>Créer une annonce</Button>
+      </ButtonGroup>
+      {isUserProfile && <UserProfile />}
+
+    </Container>
   );
 };
 

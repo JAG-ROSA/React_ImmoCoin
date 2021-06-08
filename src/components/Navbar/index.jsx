cf789447fe15a84a74727e95e292c8415f312b63
carolemny <meneycarole@gmail.com>
2021-06-08 18:09:23 +0200

    Conditionnal rendering on Navbar and home

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,24 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const Navbar = () => (
-  <header>
-    <nav>
-      <div className="container">
-        <div className="d-flex justify-content-between align-items-baseline">
-          <h1 className="navbar-brand"><Link to="/">ImmoCoin</Link></h1>
-          <div className="col-3">
-            <ul className="d-flex justify-content-between">
-              <li><Link to="/register">S'inscrire</Link></li>
-              <li><Link to="/user">Profil</Link></li>
-              <li><Link to="/login">Se connecter</Link></li>
-              <li><Link to="/logout">Se déconnecter</Link></li>
+const Navbar = () => {
+  const auth = useSelector((store) => store.isLogged);
+
+  return (
+    <header>
+      <nav>
+        <div className="container">
+          <div className="d-flex justify-content-between align-items-baseline">
+            <h1 className="navbar-brand">
+              <Link to="/">ImmoCoin</Link>
+            </h1>
+            <ul className="d-flex">
+              {!auth ? (
+                <>
+                  <li className="pe-3">
+                    <Link to="/register">S'inscrire</Link>
+                  </li>
+                  <li className="pe-3">
+                    <Link to="/login">Se connecter</Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="pe-3">
+                    <Link to="/user">Profil</Link>
+                  </li>
+                  <li className="pe-3">
+                    <Link to="/logout">Se déconnecter</Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
-      </div>
-    </nav>
-  </header>
-);
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;

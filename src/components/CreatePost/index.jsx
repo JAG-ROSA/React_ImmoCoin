2d4c2c171fb884b5f78f254e3b51a8866be8094a
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-09 10:27:24 +0200

    add Post input forms

--- src/components/CreatePost/index.jsx
+++ src/components/CreatePost/index.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const CreatePost = () => (
-    <div> 
-      Test
-    </div>
+  <div> 
+    <Form>
+      <Form.Group controlId="formTitle">
+        <Form.Label>Titre</Form.Label>
+        <Form.Control type="text" placeholder="Donnez un titre à votre annonce" />
+      </Form.Group>
+
+      <Form.Group controlId="formDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="text" placeholder="Veuillez décrire votre bien" />
+      </Form.Group>
+      
+      <Form.Group controlId="formPrice">
+        <Form.Label>Prix</Form.Label>
+        <Form.Control type="" placeholder="Veuillez donner un prix à votre bien" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  </div>
 );
 
 export default CreatePost;

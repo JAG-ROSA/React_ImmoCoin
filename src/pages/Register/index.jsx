aff517c4cb8781bbc5500507a17007fde5b858d8
m-tessier <morgane.tessier22@gmail.com>
2021-06-09 15:05:04 +0200

    update front pages Login and Register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -11,40 +11,57 @@ const Register = () => {
   
   const registerFetch = (event) => {
     event.preventDefault();
+
     const data = {
       email: event.target.formBasicEmail.value,
       password: event.target.formBasicPassword.value,
     };
-    UserManager.registerUser(data.email, data.password)
-      .then(() => {
-        dispatch(registrationSuccess());
-        UiManager.openNotification("success", "Enregistrement réussi !");
-        history.push("/");
-      })
-      .catch((error) => {
-        dispatch(registrationFailed(error.message));
-        UiManager.openNotification("error", "Enregistrement échoué !");
-      });
+
+    if (event.target.formBasicPassword.value === event.target.formBasicPassword2.value) {
+      UserManager.registerUser(data.email, data.password)
+        .then(() => {
+          dispatch(registrationSuccess());
+          UiManager.openNotification("success", "Bienvenue ! 🙂");
+          history.push("/");
+        })
+        .catch((error) => {
+          dispatch(registrationFailed(error.message));
+          UiManager.openNotification("error", "Enregistrement échoué !");
+        });
+
+    } else {
+      UiManager.openNotification("error", "Enregistrement échoué, les mots de passes ne sont pas identiques !");
+    }
   };
 
   return (
     <Container>
-      <h2>Sign up</h2>
-      <Form onSubmit={registerFetch}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="col-sm-5 col-lg-4 my-bg-light border-quaternary p-4 my-5">
+          
+          <h2 className=" my-text-tertiary">S'inscrire</h2>
+          
+          <Form onSubmit={registerFetch}>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+            <Form.Group controlId="formBasicEmail" className="pb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control size="sm" type="email" placeholder="name@example.com" />
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group controlId="formBasicPassword" className="pb-3">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control size="sm" type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword2">
+              <Form.Label>Confirmer le mot de passe</Form.Label>
+              <Form.Control size="sm" type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="btn btn-secondary mt-4 mb-3">S'inscrire</Button>
+          </Form>
+        
+        </div>
+      </div>
     </Container>
   );
 };

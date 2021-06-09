1752cdd88e912d83af564020e61be44a886bec25
m-tessier <morgane.tessier22@gmail.com>
2021-06-09 20:29:24 +0200

    update front home page

--- src/components/PropertyCard/index.jsx
+++ src/components/PropertyCard/index.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Card, CardDeck } from "react-bootstrap";
+import { Row, Card } from "react-bootstrap";
 
 const PropertyCard = ({data}) => {
   return (
-    <Container style={{ textAlign: "center" }} className="pb-5">
-      <CardDeck>
-      {data.map((property)=>{
-      return(
-        <Card key={property.id}>
-          <Card.Body>
-            <Card.Title>{property.title}</Card.Title>
-            <Card.Text>{`Descrition : ${property.description}`}</Card.Text>
-            <Card.Text>{`Prix : ${property.price} €`}</Card.Text>
-            <Button><Link to={{pathname: `/properties/${property.id}`}}>Voir la propriété</Link></Button>
-          </Card.Body>
-        </Card>
-      )
-      })}      
-      </CardDeck>
-    </Container>
+    <div className="d-flex">
+      <Row>
+        {data.map((property) => (
+          <div className="col-4 py-2">
+            <Card key={property.id}>
+              <Card.Body>
+                <Card.Title>{property.title}</Card.Title>
+                <Card.Text>{`Descrition : ${property.description}`}</Card.Text>
+                <Card.Text>{`Prix : ${property.price} €`}</Card.Text>
+                <Link to={{pathname: `/properties/${property.id}`}} className="btn btn-sm">Voir la propriété</Link>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}      
+      </Row>
+    </div>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -32,56 +32,27 @@ const Home = () => {
 
   return (
     <div className="pb-5">
-      <Jumbotron
-        fluid
-        style={{
-          backgroundImage: `url(${BannerImage})`,
-          height: "80vh",
-          width: "100%",
-          backgroundSize: "cover",
-          backgroundPosition: "center top",
-        }}
-        className="d-flex justify-content-center"
-      >
-        <Container style={{ textAlign: "center" }}>
-          <h2 className="fs-1 pt-70">Bienvenue sur ImmoCoin</h2>
-          <p className="mb-5 pb-2">
-            Facilitez-vous la vie et faites confiance à ImmoCoin
-          </p>
-          {!auth && (
-            <Link to="/login" className="btn-secondary btn-lg">
-              S'inscrire
-            </Link>
-          )}
-        </Container>
-      </Jumbotron>
-      <Container style={{ textAlign: "center" }} className="pb-5">
-        <Row style={{ heigth: "50rem" }} className="p-5">
-          <Col>
-            <Card className="my-bg-light">
-              <Card.Body>
-                <Card.Title>Propriétaires</Card.Title>
-                <Card.Text>Vous avez un logement à vendre ?</Card.Text>
-                <Button variant="primary">Je suis un propriétaire</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="my-bg-light">
-              <Card.Body>
-                <Card.Title>Particuliers</Card.Title>
-                <Card.Text>Vous avez un logement à louer/acheter ?</Card.Text>
-                <Button variant="primary">Je suis un particulier</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
 
-        <SearchBar data={propertiesList} filtered={searchBarResult}/>
-        <p>Consultez la liste de nos annonces</p>
+      <Jumbotron fluid className="jumbotron d-flex justify-content-center pb-5">
+        <div className="text-center">
+          <h2 className="fs-1 pt-70 mb-2">Bienvenue sur ImmoCoin</h2>
+          <p className="mb-5 pb-2">Facilitez-vous la vie et faites confiance à ImmoCoin</p>
+          {auth
+            ? <Link to="/user/me" className="btn-secondary btn-lg">Je suis un propriétaire</Link>
+            : <Link to="/login" className="btn-secondary btn-lg">Je suis un propriétaire</Link>
+          }
+          <a href="#search" className="btn-secondary btn-lg ms-3">Je recherche un bien</a>
+        </div>
+      </Jumbotron>
+
+      <Container id="search" className=" py-5">
+        <div className="text-center"  >
+          <h2 className="my-text-tertiary">Consultez la liste de nos annonces</h2>
+          <SearchBar data={propertiesList} filtered={searchBarResult}/>
+        </div>
         <div className="mt-5">
-        <PropertyCard data={filteredData} />
-      </div>
+          <PropertyCard data={filteredData} />
+        </div>
       </Container>
     </div>
   );

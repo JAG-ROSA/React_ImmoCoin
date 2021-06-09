9d65817c83f3a1e02ab46002f4bfb897fee9469f
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-09 20:06:34 +0200

    add props in UserProfile component

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,42 +1,38 @@
 import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
-// import { UserManager } from "services";
 
-const UserProfile = ({user}) => {
-  
-  return(
-    <Form>
-      <Form.Group as={Row} controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Email
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue={user.email}/>
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} controlId="formPlaintextFirstName">
-        <Form.Label column sm="2">
-          First Name
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue={user.first_name}/>
-        </Col>
-      </Form.Group>
+const UserProfile = ({user, onUpdate}) => (
+  <Form onSubmit={onUpdate}>
+    <Form.Group as={Row} controlId="formPlaintextEmail">
+      <Form.Label column sm="2">
+        Email
+      </Form.Label>
+      <Col sm="10">
+        <Form.Control plaintext readOnly defaultValue={user.email}/>
+      </Col>
+    </Form.Group>
+    <Form.Group as={Row} controlId="formPlaintextFirstName">
+      <Form.Label column sm="2">
+        Prénom
+      </Form.Label>
+      <Col sm="10">
+        <Form.Control plaintext placeholder="Entrez votre prénom" defaultValue={user.first_name}/>
+      </Col>
+    </Form.Group>
 
-      <Form.Group as={Row} controlId="formPlaintextLastName">
-        <Form.Label column sm="2">
-          Last Name
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue={user.last_name}/>
-        </Col>
-      </Form.Group>
+    <Form.Group as={Row} controlId="formPlaintextLastName">
+      <Form.Label column sm="2">
+        Nom
+      </Form.Label>
+      <Col sm="10">
+        <Form.Control plaintext placeholder="Entrez votre nom" defaultValue={user.last_name}/>
+      </Col>
+    </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Save Change
-      </Button>
-    </Form>
-  );
-};
+    <Button variant="primary" type="submit">
+      Save Change
+    </Button>
+  </Form>
+);
 
 export default UserProfile;

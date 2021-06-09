2e0ecc7f3c4451af6f4cfd7a19e949a4b74cb17c
martinfzz <forget.l.martin@gmail.com>
2021-06-09 09:23:20 +0200

    Add side input in slide bar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Slider, Switch } from 'antd';
+import { Slider, Switch, InputNumber, Row, Col } from 'antd';
 
 const SearchBar = ({data}) => {
   const [filteredData, setFilteredData] = useState([])
-  const [disabled, setDisabled] = useState(true)
+  const [disabled, setDisabled] = useState(false)
+  const [inputValueMin, setInputValueMin] = useState(100000)
+  const [inputValueMax, setInputValueMax] = useState(500000)
 
   useEffect(() =>{
     setFilteredData(data)
@@ -25,13 +27,50 @@ const SearchBar = ({data}) => {
     setDisabled(disabled)
   }
 
+  const onChangeMin = (value) => {
+    setInputValueMin(value)
+  };
+
+  const onChangeMax = (value) => {
+    setInputValueMax(value)
+  };
+
+  const onChange = (value) => {
+    setInputValueMin(value[0])
+    setInputValueMax(value[1])
+  };
+
   return (
     <div className="App">
       <div style={{ margin: '0 auto', marginTop: '10%' }}>
         <label>Search title:</label>
         <input id="titleSearch" type="text" onChange={(e) =>handleSearch(e)} />
       </div>
-      <Slider range min={0} max={1000000} step={1000}defaultValue={[100000, 500000]} disabled={disabled} />
+      <Row>
+      <Col span={4}>
+          <InputNumber
+            min={1}
+            max={1000000}
+            step={1000}
+            style={{ margin: '0 16px' }}
+            value={inputValueMin}
+            onChange={onChangeMin}
+          />
+        </Col>
+        <Col span={12}>
+      <Slider range min={0} max={1000000} step={1000} value={[inputValueMin, inputValueMax]} disabled={disabled} onChange={onChange}/>
+        </Col>
+        <Col span={4}>
+          <InputNumber
+            min={1}
+            max={1000000}
+            step={1000}
+            style={{ margin: '0 16px' }}
+            value={inputValueMax}
+            onChange={onChangeMax}
+          />
+        </Col>
+      </Row>
       Disabled: <Switch size="small" checked={disabled} onChange={handleDisabledChange} />
 
       <div style={{padding:10}}>
@@ -41,6 +80,7 @@ const SearchBar = ({data}) => {
 
       {value.title}<br/>
       {value.description}<br/><br/>
+      {value.description.price}<br/>
       </div>
       )
       })}

1bd748f73a97e8e6a202f91a39669e1881acd66b
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-09 20:08:37 +0200

    add upateUserProfile function

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,41 +3,49 @@ import { Container } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import UserProfile from "components/UserProfile";
 import CreatePost from "components/CreatePost";
-import { UserManager } from "services";
+import { UiManager, UserManager } from "services";
 // import "./style.scss";
 
 const Profile = () => {
   const [isUserProfile, setIsUserProfile] = useState(false);
   const [isCreatePost, setIsCreatePost] = useState(false);
   const [userDetails, setUserDetails] = useState(undefined); 
+  const [updateProfile, setUpdateProfile] = useState([]);
 
-  const onUserProfile = () => {
+  const onUserProfile = (event) => {
+    event.preventDefault();
     setIsUserProfile(true);
     setIsCreatePost(false);
   };
-
-  console.log(isUserProfile);
-  console.log(isCreatePost);
+  
   const onCreatePost = (event) => {
     event.preventDefault();
     setIsUserProfile(false);
     setIsCreatePost(true);
   };
 
+  const updateUserProfile = (event) => {
+    event.preventDefault();
+    const data = {
+      first_name: event.target.formPlaintextFirstName.value,
+      last_name: event.target.formPlaintextLastName.value,
+    };
+    UserManager.updateProfile(data)
+      .then((response) => {
+        setUpdateProfile(response);
+        UiManager.openNotification("success", "Mise à jour réussie !");
+    });
+  };
+
   useEffect(() => {
     UserManager.getProfile().then((response) => {
       setUserDetails(response);
-      console.log(`useEffect: ${userDetails}`);
     });
-    UserManager.getProfile().then((response) => {
-      setUserDetails(response);
-      console.log(`useEffect: ${userDetails}`);
-    });
-  },[]);
+  },[updateProfile]);
 
   return (
     <Container>
-      <h2> User Dashboard </h2>
+      <h2> Bienvenue sur votre Dashboard </h2>
       <div className="d-flex align-items-start">
         <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
           <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={onUserProfile}>Profil Utilisateur</button>
@@ -46,7 +54,7 @@ const Profile = () => {
           <button className="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
         </div>
         <div className="tab-content" id="v-pills-tabContent">
-          <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{isUserProfile && <UserProfile user={userDetails}/>}</div>
+          <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{isUserProfile && <UserProfile user={userDetails} onUpdate={updateUserProfile}/>}</div>
           <div className="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">{isCreatePost && <CreatePost />}</div>
           <div className="tab-pane fade show active" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"></div>
         </div>

2eead571e36842cf76b9613e6fa54f77e8c445ca
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-09 13:07:54 +0200

    update user profile component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,13 +1,8 @@
-import { Menu } from "antd";
-import Layout, { Content, Footer, Header } from "antd/lib/layout/layout";
-import Sider from "antd/lib/layout/Sider";
-import CreatePost from "components/CreatePost";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+// import { Link } from "react-router-dom";
 import UserProfile from "components/UserProfile";
-import React, {  useState, useEffect } from "react";
-// import React from "react";
-import { Button, Container } from "react-bootstrap";
-// import { Button, Container, Col} from "react-bootstrap";
-// import { useSelector } from "react-redux";
+import CreatePost from "components/CreatePost";
 import "./style.scss";
 
 const Profile = () => {
@@ -34,27 +29,25 @@ const Profile = () => {
 
   return (
     <Container>
-      <Header theme="light">
-        <h2> User Dashboard </h2>
-      </Header>
-      <Layout>
-        <Sider theme="light">
-          <Menu defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" onClick={onUserProfile}>
-              Profil Utilisateur
-            </Menu.Item>
-            <Menu.Item key="2" onClick={onCreatePost}>
-              Créer une annonce
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content>
-          {isUserProfile && <UserProfile/>}
-          {isCreatePost && <CreatePost/>}
-        </Content>
-      </Layout>
+      <h2> User Dashboard </h2>
+      <div className="d-flex align-items-start">
+        <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={onUserProfile}>Profil Utilisateur</button>
+          <button className="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false" onClick={onCreatePost}>Créer une annonce</button>
+          <button className="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+          <button className="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+        </div>
+        <div className="tab-content" id="v-pills-tabContent">
+          <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{isUserProfile && <UserProfile />}</div>
+          <div className="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">{isCreatePost && <CreatePost />}</div>
+          <div className="tab-pane fade show active" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"></div>
+        </div>
+      </div>
+
+      
     </Container>
   );
 };
 
 export default Profile;
+

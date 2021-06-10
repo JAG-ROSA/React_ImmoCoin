230013caf168f1a0f6b6430b1e350d3411ba94e4
m-tessier <morgane.tessier22@gmail.com>
2021-06-10 15:56:33 +0200

    add resposive to pages devise

--- src/pages/ForgotPassword/index.jsx
+++ src/pages/ForgotPassword/index.jsx
@@ -27,7 +27,7 @@ const ForgotPassword = () => {
   return (
     <Container>
       <div className="d-flex justify-content-center align-items-center">
-        <div className="col-sm-5 col-lg-4 my-bg-light border-quaternary p-4 my-5">
+        <div className="col col-md-8 col-lg-5 my-bg-light border-quaternary p-4 my-5 mx-3">
           
           <h2 className=" my-text-tertiary">Mot de passe oublié</h2>
           

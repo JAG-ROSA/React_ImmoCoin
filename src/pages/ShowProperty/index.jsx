34bf4c59ce10a36dd47d4e14355eadd686aaaa8c
carolemny <76789941+carolemny@users.noreply.github.com>
2021-06-10 16:40:11 +0200

    Merge Show property (#38)
    * add show-property-page
    
    * Add alt prop to picture

--- src/pages/ShowProperty/index.jsx
+++ src/pages/ShowProperty/index.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { BsFillEnvelopeFill, BsJustifyLeft } from "react-icons/bs";
+import { RiMoneyEuroCircleLine } from "react-icons/ri";
+import { CgLogIn } from "react-icons/cg";
 import { PropertiesManager } from "services";
+import House from "assets/images/02_house.jpg";
+import "./style.scss";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ShowProperty = () => {
   const { propId } = useParams();
   const [currentProperty, setCurrentProperty] = useState(undefined);
+  const auth = useSelector((store) => store.isLogged);
 
   useEffect(() => {
     PropertiesManager.showProperties(propId)
@@ -19,11 +27,38 @@ const ShowProperty = () => {
       {currentProperty !== undefined ? (
         <div className="ShowProperty">
           <h1>{currentProperty.property.title}</h1>
-          <p>{currentProperty.property.description}</p>
-          <p>
-            <strong>{currentProperty.property.price} €</strong>
-          </p>
-          <p>Contactez le vendeur : {currentProperty.user.email}</p>
+          <div className="show-property-content">
+            <img src={House} alt={currentProperty.property.title}/>
+            <div className="show-property-infos">
+              <p className="show-property-price">
+                <RiMoneyEuroCircleLine className="my-mr my-medium" />
+                <strong>{currentProperty.property.price} €</strong>
+              </p>
+              <p className="show-property-email">
+                {auth ? (
+                  <a
+                    href={`mailto: ${currentProperty.user.email}`}
+                    className="contact-email"
+                  >
+                    <BsFillEnvelopeFill className="my-mr my-medium" />
+                    {currentProperty.user.email}
+                  </a>
+                ) : (
+                  <Link to="/login" className="contact-email">
+                    <CgLogIn className="my-medium"/> Connexion
+                  </Link>
+                )}
+                <p className="contact-label">
+                  <BsFillEnvelopeFill className="my-mr my-medium" /> Contacter le vendeur
+                </p>
+              </p>
+            </div>
+            <hr />
+            <h4>
+              <BsJustifyLeft /> Description
+            </h4>
+            <p>{currentProperty.property.description}</p>
+          </div>
         </div>
       ) : (
         <div className="NoProperty">

906d27c485222a36c1973f0d7441ff919ccbb793
m-tessier <morgane.tessier22@gmail.com>
2021-06-10 20:36:26 +0200

    update front dashboard

--- src/pages/EditProperty/index.jsx
+++ src/pages/EditProperty/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import {  Container, Form, Row, Col } from "react-bootstrap";
 import { useLocation } from "react-router";
 import { PropertiesManager, UiManager } from "services";
 
@@ -12,32 +12,50 @@ const EditProperty = () => {
     const title = event.target.formTitleEdit.value;
     const description = event.target.formDescriptionEdit.value;
     const price = event.target.formPriceEdit.value;
-    PropertiesManager.updateProperties(title, description, price, data.data);
+    PropertiesManager.updateProperties(title, description, price, data.data.id);
     UiManager.openNotification("success", " Annonce mise à jour !");
   };
 
   return (
     <Container>
-      <Form onSubmit={editProperty}>
-      <Form.Group controlId="formTitleEdit">
-        <Form.Label>Titre</Form.Label>
-        <Form.Control type="text" placeholder="Donnez un titre à votre annonce" />
-      </Form.Group>
+      <div className="d-flex justify-content-center">
+        <div className="col col-md-10 col-lg-6 my-bg-light border-quaternary p-4 my-5 mx-3">
+          <h2 className=" my-text-tertiary">Modifier mon annonce</h2>
+          <Form onSubmit={editProperty}>
+            <Form.Group controlId="formTitleEdit" className="pb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control size="sm" type="text" defaultValue={data.data.title} />
+            </Form.Group>
 
-      <Form.Group controlId="formDescriptionEdit">
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Veuillez décrire votre bien" />
-      </Form.Group>
-      
-      <Form.Group controlId="formPriceEdit">
-        <Form.Label>Prix</Form.Label>
-        <Form.Control type="" placeholder="Veuillez donner un prix à votre bien" />
-      </Form.Group>
+            <Form.Group controlId="formDescriptionEdit">
+              <Form.Label>Description</Form.Label>
+              <Form.Control size="sm" type="text" defaultValue={data.data.description} />
+            </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group controlId="formCategory">
+              <Form.Label column sm="2">Catégorie</Form.Label>
+              <Form.Control className="form-select form-select-sm" as="select">
+                <option>Appartement</option>
+                <option>Maison</option>
+                <option>Studio</option>
+                <option>Villa</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group  controlId="formLocation">
+              <Form.Label column sm="2">Lieu</Form.Label>
+              <Form.Control size="sm" type="text" placeholder="Entrez votre lieu"/>
+            </Form.Group>
+
+            <Form.Group controlId="formPriceEdit">
+              <Form.Label>Prix</Form.Label>
+              <Form.Control size="sm" type=""  defaultValue={data.data.price} />
+            </Form.Group>
+
+            <button type="submit" className="btn btn-secondary mt-4 mb-3">Modifier</button>
+          </Form>
+        </div>
+      </div>
     </Container>
   );
 };
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -49,9 +49,14 @@ const Profile = () => {
     const price = event.target.formPrice.value;
     PropertiesManager
       .createProperties(title, description, price)
-      .then((response) => {
+      .then(() => {
         getUserProfile();
         UiManager.openNotification("success", "Annonce créée !")
+      })
+      .catch((error) => {
+        UiManager.openNotification(
+          "error", "Hum... il y a une petite erreur! 🤔"
+        );
       });
   };
 
@@ -61,21 +66,26 @@ const Profile = () => {
 
   return (
     <Container>
-      <h2> Bienvenue sur votre Dashboard </h2>
-      <div className="d-flex align-items-start">
-        <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-          <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={(event) => onUserClick(event, true, false, false)}>Profil Utilisateur</button>
-          <button className="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false" onClick={(event) => onUserClick(event, false, true, false)}>Créer une annonce</button>
-          <button className="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false" onClick={(event) => onUserClick(event, false, false, true)}>Liste des annonces</button>
-        </div>
-        <div className="tab-content" id="v-pills-tabContent">
-          <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{isUserProfile && <UserProfile userDetails={userDetails} onUpdate={updateUserProfile}/>}</div>
-          <div className="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">{isCreatePost && <CreatePost onCreate={createPost} />}</div>
-          <div className="tab-pane fade show active" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">{isPropertiesList && <PropertiesList userDetails={userDetails} onDelete={deleteProperty}/>}</div>
-        </div>
-      </div>
+      <div className="py-5">
 
-      
+        <h2 className="text-center pb-3">Bienvenue sur votre Dashboard</h2>
+
+        <div className="d-flex justify-content-center">
+          <div className="col-11 d-flex justify-content-center my-bg-light my-border border-quaternary p-5">
+            <div className="col-4 col-sm-5 col-lg-3 nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <button className="nav-link link-tertiary fs-6 px-0 pb-0" type="button" role="tab" aria-controls="v-pills-home" aria-selected="false" onClick={(event) => onUserClick(event, true, false, false)}>Profil Utilisateur</button>
+              <button className="nav-link link-tertiary fs-6 px-0 pb-0" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false" onClick={(event) => onUserClick(event, false, true, false)}>Créer une annonce</button>
+              <button className="nav-link link-tertiary fs-6 px-0 pb-0" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false" onClick={(event) => onUserClick(event, false, false, true)}>Liste des annonces</button>
+            </div>
+
+            <div className="col-8 col-sm-7 col-lg-9 tab-content" id="v-pills-tabContent">
+              <div className="show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{isUserProfile && <UserProfile userDetails={userDetails} onUpdate={updateUserProfile}/>}</div>
+              <div className="show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">{isCreatePost && <CreatePost onCreate={createPost} />}</div>
+              <div className="show active" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">{isPropertiesList && <PropertiesList userDetails={userDetails} onDelete={deleteProperty}/>}</div>
+            </div>
+          </div>
+        </div>
+      </div>   
     </Container>
   );
 };

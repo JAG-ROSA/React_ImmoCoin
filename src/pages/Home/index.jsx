0fbf3f4e85335c9b566d119e3cacaec07d5f2764
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-10 11:03:32 +0200

    Front search home (#32)
    * fixed issue Strict Mode
    
    * fixed and resposive page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import SearchBar from "components/SearchBar";
 import { useSelector } from "react-redux";
 import { PropertiesManager } from "services";
 import PropertyCard from 'components/PropertyCard';
+import './style.scss';
 
 const Home = () => {
   const [propertiesList, setPropertiesList] = useState([]);
@@ -30,22 +31,24 @@ const Home = () => {
   };
 
   return (
-    <div className="pb-5">
+    <div className="pb-5 mb-5">
 
       <Jumbotron fluid className="jumbotron d-flex justify-content-center pb-5">
-        <div className="text-center">
-          <h2 className="fs-1 pt-70 mb-2">Bienvenue sur ImmoCoin</h2>
-          <p className="mb-5 pb-2">Facilitez-vous la vie et faites confiance à ImmoCoin</p>
-          {auth
-            ? <Link to="/user/me" className="btn-secondary btn-lg">Je suis un propriétaire</Link>
-            : <Link to="/login" className="btn-secondary btn-lg">Je suis un propriétaire</Link>
-          }
-          <a href="#search" className="btn-secondary btn-lg ms-3">Je recherche un bien</a>
+        <div className="text-center pt-70">
+          <h2 className="fs-1 mb-2">Bienvenue sur ImmoCoin</h2>
+          <p className="mb-3">Facilitez-vous la vie et faites confiance à ImmoCoin</p>
+          <div className="d-flex justify-content-center flex-wrap">
+            {auth
+              ? <Link to="/user/me" className="btn-secondary btn-lg mt-2">Je suis un propriétaire</Link>
+              : <Link to="/login" className="btn-secondary btn-lg mt-2">Je suis un propriétaire</Link>
+            }
+            <a href="#search" className="btn-secondary btn-lg ms-3 mt-2">Je recherche un bien</a>
+          </div>
         </div>
       </Jumbotron>
 
-      <Container id="search" className=" py-5">
-        <div className="text-center"  >
+      <Container id="search">
+        <div className="text-center pt-5">
           <h2 className="my-text-tertiary">Consultez la liste de nos annonces</h2>
           <SearchBar data={propertiesList} filtered={searchBarResult}/>
         </div>
